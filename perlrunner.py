45e00101b06a5f388b07b63eb423e4d979cdcb5a
Harsh Shah <shahharsh2603@gmail.com>
2016-02-06 23:52:15 -0500

    Removed unnecessary files

--- perlrunner.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-var1 = "Breaking News! $50 off 4on4 street hockey registration. Register by Feb 9 and enter promo code:"
-var2 = "20140129"
-pipe = subprocess.Popen(["./testTimex.pl", var1], stdout=subprocess.PIPE)
-
-result = pipe.stdout.read()
-
-print result
